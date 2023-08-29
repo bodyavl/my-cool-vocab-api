1098a3ca2ccf8d3d2e4dabf5bc23b1a81af950ec
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-08-29 19:55:18 +0300

    word module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { WordModule } from './word/word.module';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    WordModule,
   ],
   controllers: [],
   providers: [],
